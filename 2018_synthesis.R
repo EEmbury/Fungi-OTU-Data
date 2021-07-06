b03e77343142a98d877a2daff29c3fc6a419ed34
Emily Embury <emmyem9@gmail.com>
2021-07-06 17:32:13 -0400

    base code

--- 2018_synthesis.R
+++ 2018_synthesis.R
@@ -1,3 +1,18 @@
+# code source: https://grunwaldlab.github.io/analysis_of_microbiome_community_data_in_r/07--diversity_stats.html
+
+
+#### set up ####
+#clear environment
+rm(list = ls())
+
+
+#load packages
+library(vegan)
+library(tidyverse)
+library(dplyr)
+
+
+#Basic COde####
 data <- read_csv("data.csv")
 
 #Calculate alpha + add new column
@@ -6,3 +21,6 @@ data$alpha <- diversity(data,
                                      index = "shannon")
 
 write.csv(data,"name.csv", row.names = FALSE)
+
+
+#####Alpha diversity analysis####
