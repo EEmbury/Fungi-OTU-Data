175a7e410bbae60f2994e3802fbac4c56ba90b5f
Emily Embury <emmyem9@gmail.com>
2021-07-08 15:24:42 -0400

    beta diversity and mds

--- DOD2017.R
+++ DOD2017.R
@@ -68,7 +68,7 @@ summary(anova_result)
 ### Still working this out
 
 
-#### Beta Diversity ####
+#### Beta Diversity attempts ####
 
 DOD2017_combined <- read_csv("DOD2017_combined_by_type.csv")
 
@@ -86,6 +86,86 @@ install.packages("betapart")
   library(betapart)
 
 
-beta.data <- beta.sample.abund(DOD2017_combined, index.family="bray")
+beta.data <- beta.multi(DOD2017_combined, index.family="bray")
 
+
+install.packages("adespatial")
+library(adespatial)
+
+beta.data.2 <- beta.div(
+  DOD2017_combined,
+  method = "hellinger",
+  sqrt.D = FALSE,
+  samp = TRUE,
+  nperm = 999,
+  adj = TRUE,
+  save.D = FALSE,
+  clock = FALSE
+)
+
+
+
+
+#### actual beta ####
+
+#invaded
+DOD2017_invaded <- read_csv("DOD2017_invaded_byrow.csv")
+
+beta_dist_invaded <- vegdist(DOD2017_invaded,
+                     index = "bray")
+
+#uninvaded
+DOD2017_uninvaded <- read_csv("DOD2017_uninvaded_byrow.csv")
+
+beta_dist_uninvaded <- vegdist(DOD2017_uninvaded,
+                             index = "bray")
+
+#eradicated
+DOD2017_eradicated <- read_csv("DOD2017_eradicated_byrow_corrected.csv")
+
+beta_dist_eradicated <- vegdist(DOD2017_eradicated,
+                               index = "bray")
+
+
+### mds code  ####
+
+# invaded
+
+mds_invaded <- metaMDS(beta_dist_invaded)
+
+mds_data_invaded <- as.data.frame(mds_invaded$points)
+
+mds_data_invaded$SampleID <- rownames(mds_data_invaded)
+
+
+library(ggplot2)
+ggplot(mds_data_invaded, aes(x = MDS1, y = MDS2)) +
+  geom_point()
+
+#uninvaded
+
+mds_uninvaded <- metaMDS(beta_dist_uninvaded)
+
+mds_data_uninvaded <- as.data.frame(mds_uninvaded$points)
+
+mds_data_uninvaded$SampleID <- rownames(mds_data_uninvaded)
+
+
+library(ggplot2)
+ggplot(mds_data_uninvaded, aes(x = MDS1, y = MDS2)) +
+  geom_point()
+
+
+# eradicated
+
+mds_eradicated <- metaMDS(beta_dist_eradicated)
+
+mds_data_eradicated <- as.data.frame(mds_eradicated$points)
+
+mds_data_eradicated$SampleID <- rownames(mds_data_eradicated)
+
+
+library(ggplot2)
+ggplot(mds_data_eradicated, aes(x = MDS1, y = MDS2)) +
+  geom_point()
 
