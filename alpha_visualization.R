6689351090eaf691e2232c85e13bb5d8f24ea081
Emily Embury <emmyem9@gmail.com>
2021-07-14 17:58:55 -0400

    2015 analysis

--- alpha_visualization.R
+++ alpha_visualization.R
@@ -3,6 +3,7 @@
 library(phyloseq)
 library(ggplot2)
 
+###DOD 2017 ####
 #read data
 otudata <- read.csv("DOD2017_otu_matix.csv")
 otutax <- read.csv("DOD2017_seperate_taxa.csv")
@@ -43,9 +44,10 @@ DOD2017 <- phyloseq(OTU, TAX)
 rownames(samdata) <-samdata$SampleID
 
 
-#setting up ggplot themes
-
 physeq1 <- merge_phyloseq(DOD2017, SAM)
+
+
+#setting up ggplot themes
 
 theme_set(theme_bw())
 pal = "Set1"
@@ -56,19 +58,15 @@ scale_fill_discrete <-  function(palname=pal, ...){
   scale_fill_brewer(palette=palname, ...)
 }
 
-#####################################################
-
-data("GlobalPatterns")
-
-
 #fix the random column
 
 df = subset(otu_data, select = -c(X) )
 
 
-plot_richness(DOD2017, measures=c( "Shannon"))
+#Plotting
 
-plot_richness(DOD2017, x="human", color="SampleType", measures=c("Chao1", "Shannon"))
+p <- plot_richness(physeq1, x="SampleType", color="SampleType", measures=c("Shannon", "Simpson", "InvSimpson"))
+p + geom_point(size=1, alpha=0.05)
 
 
 # Taxa reorganizaion
@@ -101,3 +99,91 @@ plot_richness(DOD2017, x="human", color="SampleType", measures=c("Chao1", "Shann
 #                pattern = "g__", replacement = "")
 # 
 # write.csv(y,"DOD2017_seperate_taxa.csv", row.names = FALSE)
+
+
+
+
+#### DOD 2015 ####
+
+
+library(phyloseq)
+library(ggplot2)
+
+###DOD 2017 ####
+#read data
+otudata <- read.csv("DOD2015_data.csv")
+otutax <- read.csv("DOD2015_taxa.csv")
+samdata <- read.csv("DOD2015_metadata.csv")
+
+#coerce matrix
+
+test <- as.matrix(otu_taxa, rownames.force = NA)
+
+#change row labels
+
+otudata$OTUid <- sub(otudata$OTUid, 
+                        pattern = "OTU_", replacement = "")
+
+library(tidyverse)
+otu_data <- otudata %>% remove_rownames %>% column_to_rownames(var="OTUid")
+
+
+otutax$OTUid <- sub(otutax$OTUid, 
+                  pattern = "OTU", replacement = "")
+
+otu_taxa <- otutax %>% remove_rownames %>% column_to_rownames(var="OTUid")
+
+
+
+
+
+# Taxa reorganizaion
+
+
+ library(tidyr)
+y <- otu_taxa %>% separate(Taxonomy, c("domain", "phylum", "class", "order", "family", "genus"), "[][a-z ];")
+
+
+
+y$domain <- sub(y$domain,  pattern = "k_", replacement = "")
+
+
+y$phylum <- sub(y$phylum, 
+                pattern = "p__", replacement = "")
+ 
+ y$class <- sub(y$class, 
+                 pattern = "c__", replacement = "")
+
+ y$order <- sub(y$order,
+                pattern = "o__", replacement = "")
+
+y$family <- sub(y$family,
+               pattern = "f__", replacement = "")
+ y$genus <- sub(y$genus,
+                pattern = "g__", replacement = "")
+
+write.csv(y,"DOD2015_seperate_taxa.csv", row.names = FALSE)
+
+
+# setting data to phyloseq format
+
+library(phyloseq)
+OTU <- otu_table(otu_data, taxa_are_rows = TRUE)
+TAX <-  tax_table(test)
+SAM <- sample_data(samdata)
+OTU
+TAX
+SAM
+
+DOD2017 <- phyloseq(OTU, TAX)
+
+#mystery answer to error in validobject
+
+rownames(samdata) <-samdata$SampleID
+
+
+physeq1 <- merge_phyloseq(DOD2017, SAM)
+
+
+p <- plot_richness(physeq1, x="SampleType", color="SampleType", measures=c("Shannon", "Simpson", "InvSimpson"))
+p + geom_point(size=1, alpha=0.05)
