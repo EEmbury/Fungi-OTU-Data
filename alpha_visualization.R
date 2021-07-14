87765b0c063125efd5449ffdb2bce7ccdfe1e255
Emily Embury <emmyem9@gmail.com>
2021-07-14 15:06:26 -0400

    it works!!!!!

--- alpha_visualization.R
+++ alpha_visualization.R
@@ -1,9 +1,13 @@
 library(phyloseq)
 library(ggplot2)
 
+#read data
 otudata <- read.csv("DOD2017_otu_matix.csv")
-otutax <- read.csv("DOD2017_taxa.csv")
+otutax <- read.csv("DOD2017_seperate_taxa.csv")
 
+test <- as.matrix(otu_taxa, rownames.force = NA)
+
+#change row labels
 
 otudata$`OUT_ID` <- sub(otudata$OUT_ID, 
                          pattern = "OTU", replacement = "")
@@ -11,21 +15,51 @@ otudata$`OUT_ID` <- sub(otudata$OUT_ID,
 library(tidyverse)
 otu_data <- otudata %>% remove_rownames %>% column_to_rownames(var="OUT_ID")
 
+
+otutax$OTU <- sub(otutax$OTU, 
+                        pattern = "OTU", replacement = "")
+
+otu_taxa <- otutax %>% remove_rownames %>% column_to_rownames(var="OTU")
+
 library(phyloseq)
 OTU <- otu_table(otu_data, taxa_are_rows = TRUE)
-TAX <-  tax_table(otutax)
+TAX <-  tax_table(test)
 OTU
 TAX
 
-
-library(taxa)
-taxa <- parse_tax_data(otutax,
-                       class_cols = "Taxonomy", # The column in the input table
-                      class_sep = ";") # What each taxon is seperated by
-print(taxa)
-
-
 physeq = phyloseq(OTU, TAX)
 
-library(tidyr)
-y <- otutax %>% separate(Taxonomy, c("domain", "phylum", "class", "order", "family", "genus"), "[]_[a-z];")
+
+
+
+
+# Taxa reorganizaion
+# library(taxa)
+# taxa <- parse_tax_data(otutax,
+# #                        class_cols = "Taxonomy", # The column in the input table
+# #                       class_sep = ";") # What each taxon is seperated by
+# print(taxa)
+# 
+
+# library(tidyr)
+# y <- otutax %>% separate(Taxonomy, c("domain", "phylum", "class", "order", "family", "genus"), "[][a-z ];")
+# 
+# y$domain <- sub(y$domain, 
+#                         pattern = "d__", replacement = "")
+# 
+# y$phylum <- sub(y$phylum, 
+#                 pattern = "p__", replacement = "")
+# 
+# y$class <- sub(y$class, 
+#                 pattern = "c__", replacement = "")
+# 
+# y$order <- sub(y$order, 
+#                pattern = "o__", replacement = "")
+# 
+# y$family <- sub(y$family, 
+#                pattern = "f__", replacement = "")
+# 
+# y$genus <- sub(y$genus, 
+#                pattern = "g__", replacement = "")
+# 
+# write.csv(y,"DOD2017_seperate_taxa.csv", row.names = FALSE)
