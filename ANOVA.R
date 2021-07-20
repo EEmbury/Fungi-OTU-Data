96201c6d6f802cbb1c7590488a9be299a94456a4
Emily Embury <emmyem9@gmail.com>
2021-07-20 18:17:33 -0400

    workings ANOVA

--- ANOVA.R
+++ ANOVA.R
@@ -79,37 +79,23 @@ mds_data.17 <- dplyr::left_join(mds_data_2017, samdata.2017)
 
 ########### attempt 3 ##########
 
+##### https://rfunctions.blogspot.com/2016/08/measuring-and-comparing-beta-diversity.html
+
 library(dplyr)
 
 otudata.2017 <- read.csv("DOD2017_otu_matix_t.csv")
 
+otu_data.2017 <- otudata.2017 %>% remove_rownames %>% column_to_rownames(var="SampleID")
 
-otu_data.2017 <- otudata.2017 %>% remove_rownames %>% column_to_rownames(var="OUT_ID")
-
-otu_data.2017.t <- transpose(otu_data.2017)
-rownames(otu_data.2017.t) <- colnames(otu_data.2017)
-colnames(otu_data.2017.t) <- rownames(otu_data.2017)
-
-beta.data.2017 <- bray.part(t(otu_data.2017))
-
-
-mds.2017 <- metaMDS(beta.data.2017$bray)
-mds_data_2017 <- as.data.frame(mds.2017$points)
-#write.csv(mds_data_2017, "2017_beta.csv")
-
-
+beta.data.2017 <- bray.part(otu_data.2017)
 
 
 samdata.2017 <- read.csv("DOD2017_metadata.csv")
 rownames(samdata.2017) <-samdata.2017$SampleID
-# stress = 0.2746165 
 
 
-mds_data_2017$SampleID <- rownames(mds_data_2017)
-mds_data.17 <- dplyr:: nest_join(mds_data_2017, samdata.2017)
 
-
-test <- dplyr::left_join(otudata.2017, samdata.2017)
+test <- dplyr::left_join(otudata.2017, samdata.2017)  ### run this line with the name for importing the csv
 
 
 groups <- factor(samdata.2017$SampleType.GM)
@@ -118,3 +104,5 @@ groups
 bd<-betadisper(beta.data.2017$bray,groups)
 
 anova(bd)
+
+boxplot(bd)
